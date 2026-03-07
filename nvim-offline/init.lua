46e7ac2663a5b714d25d185044447c5865da0769
Alexis Leclerc <web@alxs.fr>
2026-03-07 16:49:31 +0100

    Add search grep/file in neovim vanilla

--- nvim-offline/init.lua
+++ nvim-offline/init.lua
@@ -15,7 +15,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.timeoutlen = 500
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.list = true
@@ -27,6 +27,10 @@ vim.o.confirm = true
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
+
+-- Use ripgrep for :grep
+vim.o.grepprg = 'rg --vimgrep --smart-case'
+vim.o.grepformat = '%f:%l:%c:%m'
 
 -- Colorscheme
 vim.cmd.colorscheme 'slate'
@@ -41,6 +45,37 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left spli
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower split' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper split' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right split' })
+
+-- Search: grep in cwd (like Telescope <leader>sg)
+vim.keymap.set('n', '<leader>sg', function()
+  vim.ui.input({ prompt = 'Grep > ' }, function(input)
+    if input and input ~= '' then
+      vim.cmd('silent! grep! ' .. vim.fn.shellescape(input))
+      vim.cmd 'copen'
+    end
+  end)
+end, { desc = 'Search by grep' })
+
+-- Search: find files in cwd (like Telescope <leader>sf)
+vim.keymap.set('n', '<leader>sf', function()
+  local tmpfile = vim.fn.tempname()
+
+  vim.cmd 'botright new'
+  local buf = vim.api.nvim_get_current_buf()
+  vim.fn.termopen('rg --files | fzf > ' .. vim.fn.shellescape(tmpfile), {
+    on_exit = function(_, code)
+      vim.api.nvim_buf_delete(buf, { force = true })
+      if code == 0 then
+        local result = vim.fn.readfile(tmpfile)
+        if #result > 0 and result[1] ~= '' then
+          vim.cmd('edit ' .. vim.fn.fnameescape(result[1]))
+        end
+      end
+      vim.fn.delete(tmpfile)
+    end,
+  })
+  vim.cmd 'startinsert'
+end, { desc = 'Search files' })
 
 -- Move splits with Ctrl+W+Shift+Arrows
 vim.keymap.set('n', '<C-w><S-Up>', '<C-w>K', { desc = 'Move split up' })
